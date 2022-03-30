88bf301bfa6a5dac631356ba45b34269674fa29c
sajibur-rahman <sajiburr21@gmail.com>
2022-03-30 23:18:48 +0600

    oreder review cart is done

--- src/Components/OrderReview/OrderReview.jsx
+++ src/Components/OrderReview/OrderReview.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import useCart from '../../Hooks/useCart';
 import useProducts from '../../Hooks/useProducts';
+import { addToDb } from '../../utilities/fakedb';
+import OrderProduct from '../OrderProduct/OrderProduct';
+import OrderSummary from '../OrderSummary/OrderSummary';
 import './OrderReview.css';
 
 const OrderReview = () => {
     const [products,setProducts] = useProducts();
+    const [cart,setCart] = useCart(products);
+    console.log(cart);
+
+    const handleProductQuantity = (id) => {
+        const newSelectedProduct = cart.find((product) => product.id === id);
+        newSelectedProduct.quantity += 1;
+        const newCart = newSelectedProduct ? [...cart] : [...cart,newSelectedProduct];
+        setCart(newCart)
+        addToDb(id,newSelectedProduct.quantity)
+    }
     return (
-        <div>
-            <h1>this is order review {products.length}</h1>
+        <div className='shop-container'>
+            {
+                cart.map((product) => <OrderProduct key={product.id} product={product} handleProductQuantity={handleProductQuantity}/>)
+            }
+            <OrderSummary cart={cart}/>
         </div>
     );
 };
