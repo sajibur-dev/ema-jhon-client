642c21be0723a298381425f18ac7949e05211a6a
sajibur-rahman <sajiburr21@gmail.com>
2022-03-30 20:40:04 +0600

    basic routing setup is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header/Header';
+import Inventory from './Components/Inventory/Inventory';
+import OrderReview from './Components/OrderReview/OrderReview';
 import Shop from './Components/Shop/Shop';
 
 function App() {
   return (
     <div>
+
       <Header/>
-      <Shop/>      
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/shop' element={<Shop/>}/>
+        <Route path='/review' element={<OrderReview/>}/>
+        <Route path='/inventory' element={<Inventory/>}/>
+      </Routes>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import logo from '../../images/Logo.svg';
-import './Header.css';
+import React from "react";
+import logo from "../../images/Logo.svg";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <nav className='header'>
-            <img src={logo} alt={logo} />
-            <div>
-                <a href="/shop">shop</a>
-                <a href="/review">Order Review</a>
-                <a href="/inventory">Inventory Management</a>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="header">
+      <a href="/">
+        <img src={logo} alt={logo} />
+      </a>
+      <div>
+        <a href="/shop">shop</a>
+        <a href="/review">Order</a>
+        <a href="/inventory">Inventory</a>
+        <a href="/about">about</a>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
