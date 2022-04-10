662033874156a33cec9f15205a3dfe9638b4c774
sajibur-rahman <sajiburr21@gmail.com>
2022-04-10 13:05:03 +0600

    login and sign in with icon is done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,15 @@
+import { faArrowRightFromBracket, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 import logo from "../../images/Logo.svg";
 import "./Header.css";
 
 const Header = () => {
+  const [user] = useAuthState(auth);
   return (
     <nav className="header">
       <Link to="/">
@@ -14,7 +20,11 @@ const Header = () => {
         <Link to="/review">Order</Link>
         <Link to="/inventory">Inventory</Link>
         <Link to="/about">about</Link>
-        <Link to="/login">login</Link>
+        {user ? (
+          <button style={{border:'none',backgroundColor:'#1c2b35'}} onClick={() => signOut(auth)}><FontAwesomeIcon className='logout-icon' icon={faArrowRightFromBracket} /></button>
+        ) : (
+          <Link to="/login"><FontAwesomeIcon   className='logout-icon' icon={faArrowRightToBracket}/></Link>
+        )}
       </div>
     </nav>
   );
--- src/Components/PrivateRoute/PrivateRoute.js
+++ src/Components/PrivateRoute/PrivateRoute.js
@@ -1,11 +1,13 @@
 
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const PrivateRoute = () => {
-    const location = useLocation()
-    return auth.currentUser ? <Outlet/> : <Navigate to='/login' state={{from:location}} replace/>
+    const location = useLocation();
+    const [user] =  useAuthState(auth);
+    return user ? <Outlet/> : <Navigate to='/login' state={{from:location}} replace/>
 };
 
 export default PrivateRoute;
