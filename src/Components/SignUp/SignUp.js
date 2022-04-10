e39282fd244ae6fbedd3f7507c9661ff4215d2ed
sajibur-rahman <sajiburr21@gmail.com>
2022-04-10 12:04:42 +0600

    signup is done

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [confirmPassword,setConfirmPassword] = useState('');
+
+    const [error,setError] = useState('');
+
+    const navigate = useNavigate();
+
+    const [
+        createUserWithEmailAndPassword,
+        user
+      ] = useCreateUserWithEmailAndPassword(auth);
+
+
+
+
+    if(user){
+        navigate('/shop');
+    }
+
+    const handleForm = (e) => {
+        e.preventDefault()
+        if (password !== confirmPassword) {
+            setError('you don no match');
+            return;
+        } 
+
+        createUserWithEmailAndPassword(email,password);
+    }
+
+
     return (
         <div className='form-container'>
             <div>
                 <h2 className='login-title'>Sign up</h2>
-                <form>
+                <form onSubmit={handleForm}>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="email" />
+                        <input  onBlur={(e) => setEmail(e.target.value)} type="email" name="email" id="email" />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="password" />
+                        <input onBlur={(e) => setPassword(e.target.value)} type="password" name="password" id="password" />
                     </div>
                     <div className="input-group">
-                        <label htmlFor="confirm-password">confirm password</label>
-                        <input type="password" name="confirm-password" id="confirm-password" />
+                        <label htmlFor="confirmPassword">confirm password</label>
+                        <input onBlur={(e) => setConfirmPassword(e.target.value)} type="password" name="confirmPassword" id="confirm-password" />
                     </div>
+                    <p>{error}</p>
                     <input className='form-submit' type="submit" value="Login" />
                 </form>
                 <p>Already have an account?<Link  className='signup-link' to='/login'>login</Link></p>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,25 +5,25 @@ import { getAuth } from 'firebase/auth';
  
 
 
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_ID
-  };
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_ID
+//   };
   
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDqEj74ov670LN1ZRO9nmMDIApBcBDWJTo",
-//   authDomain: "ema-jhon-simple-e6194.firebaseapp.com",
-//   projectId: "ema-jhon-simple-e6194",
-//   storageBucket: "ema-jhon-simple-e6194.appspot.com",
-//   messagingSenderId: "829455620509",
-//   appId: "1:829455620509:web:55fb31ae13d4d0a7863774"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyDqEj74ov670LN1ZRO9nmMDIApBcBDWJTo",
+  authDomain: "ema-jhon-simple-e6194.firebaseapp.com",
+  projectId: "ema-jhon-simple-e6194",
+  storageBucket: "ema-jhon-simple-e6194.appspot.com",
+  messagingSenderId: "829455620509",
+  appId: "1:829455620509:web:55fb31ae13d4d0a7863774"
+};
 
 
 
